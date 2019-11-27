7aca484cf26c8e82d4fe2390a8ac99ae95d671b4
Alex <alex.radevic@gmail.com>
2019-11-27 18:18:02 +0000

    code prittierfy

--- functions.ts
+++ functions.ts
@@ -1,27 +1,33 @@
 const add = (a: number, b: number): number => {
-    return a + b;
+  return a + b;
 };
 
 const multiply = function(a: number, b: number): number {
-    return a * b;
+  return a * b;
 };
 
 const logger = (message: string): void => {
-    console.log(message);
+  console.log(message);
 };
 
 const throwError = (message: string): never => {
-    throw new Error(message);
+  throw new Error(message);
 };
 
 const forecast = {
-    date: new Date(),
-    weather: 'sunny'
+  date: new Date(),
+  weather: "sunny"
 };
 
-const logWeather = ({date, weather}: {date: Date; weather: string}): void => {
-    console.log(date);
-    console.log(weather);
+const logWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
 };
 
 logWeather(forecast);
--- objects.ts
+++ objects.ts
@@ -1,19 +1,20 @@
 const profile = {
-    name: 'alex',
-    age: 20,
-    coords: {
-        lat: 0,
-        lng: 15
-    },
-    setAge(age: number): void {
-        this.age = age;
-    }
+  name: "alex",
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
 };
 
 // Object destructure
 const { age }: { age: number } = profile;
 console.log(`age: ${age}`);
 
-const { coords: { lat, lng } }: 
-{ coords: { lat: number; lng: number} } = profile;
+const {
+  coords: { lat, lng }
+}: { coords: { lat: number; lng: number } } = profile;
 console.log(`lat: ${lat} and lng: ${lng}`);
